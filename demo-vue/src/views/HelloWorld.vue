7d3fb26743fc0a20d38e2aab791c154f4ae1589f
inxxiu <liuxx2573@gmail.com>
2024-06-29 17:49:04 +0800

    提交说明

---
+++ demo-vue/src/views/HelloWorld.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+
+const welcomeMessage = ref('欢迎来到我们的网站！');
+const count = ref(0);
+</script>
+
+<template>
+  <div class="welcome-container">
+    <h1>{{ welcomeMessage }}</h1>
+    <h2>您是第 {{ count }} 位访问者</h2>
+
+    <div class="card">
+      <button type="button" @click="count++">点击计数</button>
+      <p>点击按钮查看访客计数。</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.welcome-container {
+  text-align: center;
+  margin-top: 100px;
+}
+
+.card {
+  margin: 0 auto;
+  width: 50%;
+  border: 1px solid #ccc;
+  padding: 20px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+</style>
